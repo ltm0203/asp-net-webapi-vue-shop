b9d7348ada2ebb7f41576b9ed3d5266777bbead6
Tongming Liang(梁桐铭) <werltm@hotmail.com>
2022-01-13 15:41:50 +0800

    完成产品控制器。

---
+++ YoYoMooc.ECommerce.API/YoYoMooc.ECommerce.API/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using YoYoMooc.ECommerce.API.Models;
+using YoYoMooc.ECommerce.API.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace YoYoMooc.ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+             _productRepository = productRepository;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+             return _productRepository.GetAllProducts(); 
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- YoYoMooc.ECommerce.API/YoYoMooc.ECommerce.API/Program.cs
+++ YoYoMooc.ECommerce.API/YoYoMooc.ECommerce.API/Program.cs
@@ -1,18 +1,15 @@
 
+using YoYoMooc.ECommerce.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+ builder.Services.AddControllers();
 
-
-
-
-   
+builder.Services.AddScoped<IProductRepository, MockProductRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.MapControllers();
-
 app.Run();
