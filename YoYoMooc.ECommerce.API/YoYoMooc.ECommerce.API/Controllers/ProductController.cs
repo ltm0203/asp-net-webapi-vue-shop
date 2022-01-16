d3bb06f3418013a58e880fdc4010228ac0bd284c
Tongming Liang(梁桐铭) <werltm@hotmail.com>
2022-01-16 18:48:05 +0800

    Swagger

--- YoYoMooc.ECommerce.API/YoYoMooc.ECommerce.API/Controllers/ProductController.cs
+++ YoYoMooc.ECommerce.API/YoYoMooc.ECommerce.API/Controllers/ProductController.cs
@@ -3,44 +3,44 @@
 using YoYoMooc.ECommerce.API.Models;
 using YoYoMooc.ECommerce.API.Services;
 
- 
 namespace YoYoMooc.ECommerce.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : Controller
     {
-
         private readonly IProductRepository _productRepository;
 
         public ProductController(IProductRepository productRepository)
         {
-             _productRepository = productRepository;
+            _productRepository = productRepository;
         }
 
         // GET: api/<ProductController>
         [HttpGet]
         public ActionResult<IEnumerable<Product>> GetAllProducts()
         {
-             return Ok(_productRepository.GetAllProducts()); 
-        }
 
- 
+            
+
+            return Ok(_productRepository.GetAllProducts());
+        }
 
         // GET api/<ProductController>/5
         [HttpGet("{id}")]
         public ActionResult<Product> GetProductById(int id)
         {
-        var product=   _productRepository.GetProductById(id);
+            var product = _productRepository.GetProductById(id);
+
+            
 
 
-            if (product!=null)
+            if (product != null)
             {
                 return product;
             }
 
             return NotFound();
-
         }
 
         // POST api/<ProductController>
@@ -48,29 +48,26 @@ namespace YoYoMooc.ECommerce.API.Controllers
         public ActionResult<Product> CreateProduct(Product product)
         {
             var newProduct = _productRepository.CreateProduct(product);
-        
+
             return Ok(newProduct);
-        
         }
 
         // PUT api/<ProductController>/5
         [HttpPut("{id}")]
         public ActionResult<Product> UpdateProduct(int id, Product updateProduct)
         {
-
-           var model=       _productRepository.GetProductById(id);
-            if (model==null)
+            var model = _productRepository.GetProductById(id);
+            if (model == null)
             {
                 return NotFound();
             }
 
-        var product=    _productRepository.UpdateProduct(updateProduct);
+            var product = _productRepository.UpdateProduct(updateProduct);
 
-          //  return NoContent();
+            //  return NoContent();
             return product;
-
         }
-         
+
         // DELETE api/<ProductController>/5
         [HttpDelete("{id}")]
         public ActionResult DeleteProduct(int id)
--- YoYoMooc.ECommerce.API/YoYoMooc.ECommerce.API/Program.cs
+++ YoYoMooc.ECommerce.API/YoYoMooc.ECommerce.API/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.OpenApi.Models;
+
 using YoYoMooc.ECommerce.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,10 +8,39 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
  builder.Services.AddControllers();
 
-builder.Services.AddScoped<IProductRepository, MockProductRepository>();
+// 注册Swagger生成器，定义一个或多个Swagger文档
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "YoYoMooc.ECommerce.API", Version = "v1" });
+});
+
+
+builder.Services.AddSingleton<IProductRepository, MockProductRepository>();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-app.MapControllers();
+
+// 启用中间件Swagger
+app.UseSwagger();
+//启用中间件Swagger -ui服务，它需要与Swagger配置在一起
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+});
+
+
+
+////添加身份认证中间件
+//app.UseAuthentication();
+app.UseRouting();
+////添加授权中间件
+//app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+//app.MapControllers();
 app.Run();
